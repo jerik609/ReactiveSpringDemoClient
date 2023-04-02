98e8b5f2ef78f4e629b5e1c7c367393ff79b9963
Michal <117671671+jerik609@users.noreply.github.com>
2023-04-02 20:53:03 +0200

    initial commit

---
+++ src/main/java/com/example/ReactiveSpringDemoClient/ReactiveSpringDemoClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.ReactiveSpringDemoClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveSpringDemoClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveSpringDemoClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ReactiveSpringDemoClient/ReactiveSpringDemoClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ReactiveSpringDemoClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveSpringDemoClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
